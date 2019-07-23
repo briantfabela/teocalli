40abfc1fe4818e6c54aaba1bf6b4f9a8753f02e0
Briant Fabela <briantfabela@umail.ucsb.edu>
2019-07-23 15:55:57 -0700

    created branch, added events txt

---
+++ event.py
@@ -0,0 +1,14 @@
+class Event:
+
+    def __init__(self):
+        '''lorem ipsum'''
+        
+        self.loot = {}
+        self.hp_change = 0
+        self.risk_change = 0
+        self.xp_gain = 0
+
+    def choose_event(self):
+        '''lorem ipsum'''
+        
+        pass
